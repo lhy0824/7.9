4837c7f8ce170d18eea27ec9010351e7e8a07d78
lhy <m18612723226@163.com>
2018-07-09 16:56:06 +0800

    完成

--- gulpfile.js
+++ gulpfile.js
@@ -16,6 +16,8 @@ var fs = require('fs');
 
 var data = require('./data/data.json');
 
+var es6 = require('gulp-babel');
+
 //起服务
 gulp.task('server', function() {
     gulp.src('src')
@@ -35,3 +37,38 @@ gulp.task('server', function() {
             }
         }))
 });
+//压缩css
+gulp.task('cleanCss', function() {
+    gulp.src('./src/css/*.css')
+        .pipe(css())
+        .pipe(gulp.dest('./build/css'))
+});
+
+//压缩js
+gulp.task('Js', function() {
+    gulp.src(['./src/js/*.js', '!./js/builJs/*.min.js'])
+        .pipe(es6({
+            presets: ['es2015']
+        }))
+        .pipe(uglify())
+        .pipe(gulp.dest('./build/js'))
+});
+var options = {
+    removeComments: true, //清除HTML注释
+    collapseWhitespace: true, //压缩HTML
+    collapseBooleanAttributes: true, //省略布尔属性的值 <input checked="true"/> ==> <input />
+    removeEmptyAttributes: true, //删除所有空格作属性值 <input id="" /> ==> <input />
+    removeScriptTypeAttributes: true, //删除<script>的type="text/javascript"
+    removeStyleLinkTypeAttributes: true, //删除<style>和<link>的type="text/css"
+    minifyJS: true, //压缩页面JS
+    minifyCSS: true //压缩页面CSS
+};
+//压缩html
+gulp.task('cssmin', function() {
+    gulp.src('./src/html/**/*.html')
+        .pipe(htmlMin(options))
+        .pipe(gulp.dest('./build/html'))
+});
+
+//整合
+gulp.task('dev', ['cssmin', 'Js', 'server', 'cleanCss'])
